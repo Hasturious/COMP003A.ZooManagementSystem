dee51488a5c315da779498fd3d0c5b9756e7a9fd
Jeanbryantfigueroa <Jeanbryantfigueroa@LEM231-31>
2025-02-19 18:18:22 -0800

    Add project files.

---
+++ COMP003A.ZooManagementSystem/Animal.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    internal class Animal
+    {
+        private _name;
+        private _species;
+
+        //Name/Species definers
+        public string Name { get; set; }
+        public string Species { get; set; }
+
+        /// <summary>
+        /// Animal constructor
+        /// </summary>
+        /// <param name="namem"></param>
+        /// <param name="species"></param>
+        public Animal(string name string species)
+        {
+            Name = name;
+            Species = species;
+        }
+    }
+}
---
+++ COMP003A.ZooManagementSystem/Lion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace COMP003A.ZooManagementSystem
+{
+    class Lion : Animal
+    {
+        public Lion(string name, string species) : base(name, species) { }
+
+        public override void MakeSound()
+        {
+            Console.WriteLine("The Lion Roars!");
+        }
+    }
+}
